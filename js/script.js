123ced2e35717d3f12e89bf25b0a1afb74285b0b
senthilkumaransu <senthilkumaransu@dpinfotech.lk>
2024-09-25 14:38:07 +0530

    job added by sugan on 25.09.2024

--- js/script.js
+++ js/script.js
@@ -56,7 +56,7 @@ ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
 
 /*==================== typed js ====================*/
 const typed = new Typed(".multiple-text", {
-  strings: ["Frontend Developer", "Youtuber", "Blogger"],
+  strings: ["Frontend Developer", "Youtuber", "Blogger", "Software Engineer"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
